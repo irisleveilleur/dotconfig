11870f8253ca3ce8b4076ab4a782f880efbdd142
Iris Leveilleur <irisleveilleur@gmail.com>
2025-06-27 23:02:29 +0200

    format nvim confing

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -1,5 +1,15 @@
 local builtin = require('telescope.builtin')
 
+require('telescope').setup{
+    defaults = {},
+    pickers = {
+        find_files = {
+            theme = "dropdown",
+            previewer = false
+        },
+    }
+}
+
 require('telescope').load_extension('fzf')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
